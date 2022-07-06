8f8d5f853881b92db74030346996031b7595fde6
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-07-06 15:49:29 +0200

    WIP 6

--- demos/selenium-test-app/lib.rs
+++ demos/selenium-test-app/lib.rs
@@ -1,4 +1,13 @@
-#[ic_cdk_macros::query]
+use ic_cdk_macros::{query, update};
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+thread_local! {
+    static STATE: String = "".to_string();
+    static ASSETS: RefCell<HashMap<&'static str, (Vec<(String, String)>, &'static [u8])>> = RefCell::new(HashMap::default());
+}
+
+#[query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
